ef94e890b4d586b0aa5646c67d66e94c30bbdcc7
eoeo0326 <eoeo0326@gmail.com>
2022-01-18 18:46:03 +0900

    Django urls.py 분류

--- Django/api/urls.py
+++ Django/api/urls.py
@@ -13,11 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+
+from django.urls import path
+from api.views import *
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-
-    path('api/', include('api.urls')),
+    path('contents/', ContentsListAPI.as_view()),
+    path('contents_review/', ContentsReviewListAPI.as_view()),
+    path('interworking/', InterworkingListAPI.as_view()),
+    path('jjim/', JjimListAPI.as_view()),
+    path('users/', UsersListAPI.as_view()),
+    path('watching_log/', WatchingLogListAPI.as_view()),
 ]
