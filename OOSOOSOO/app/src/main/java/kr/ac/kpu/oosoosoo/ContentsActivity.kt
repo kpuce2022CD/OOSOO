e69fed7c03a89518415fcac7e3626a98932622fa
Chanho <cksgh5652@gmail.com>
2022-01-28 00:46:49 +0900

    Android : ViewPager2 & AdapterView 적용(가변 수량 컨텐츠 출력 가능)

--- OOSOOSOO/app/src/main/java/kr/ac/kpu/oosoosoo/ContentsActivity.kt
+++ OOSOOSOO/app/src/main/java/kr/ac/kpu/oosoosoo/ContentsActivity.kt
@@ -1,10 +1,17 @@
 package kr.ac.kpu.oosoosoo
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
 import kotlinx.android.synthetic.main.activity_contents.*
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_item.view.*
 import org.json.JSONObject
 import retrofit2.Call
 import retrofit2.Callback
@@ -15,7 +22,9 @@ class ContentsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contents)
 
-        val call = RetrofitBuilder().callContents
+        val contentsArrayList : ArrayList<ContentInfo> = ArrayList()  //Contents 리스트 변수
+
+        val call = RetrofitBuilder().callContents  //Retrofit Call
 
         call.getContentInfo().enqueue(object : Callback<List<ContentInfo>> {
 
@@ -27,19 +36,12 @@ class ContentsActivity : AppCompatActivity() {
                 if (contents != null) {
                     for (content in contents) {
                         Log.d("movie", content.toString())
-                        content_id.text = content.c_id.toString()
-                        content_genre.text = content.c_genre.toString()
-                        content_name.text = content.c_name.toString()
-                        content_release.text = content.c_release_date.toString()
-                        content_cast.text = content.c_cast.toString()
-                        content_platform.text = content.c_id.toString()
-                        content_information.text = content.c_id.toString()
-                        content_url.text = content.c_id.toString()
-                        content_number.text = content.c_id.toString()
-
-
+                        contentsArrayList.add(content)   //Contents 리스트 셋팅
                     }
                 }
+
+                viewPager_content.adapter = ContentsViewPagerAdapter(contentsArrayList) // 어댑터 생성
+                viewPager_content.orientation = ViewPager2.ORIENTATION_HORIZONTAL // 방향을 가로로
             }
 
             override fun onFailure(call: Call<List<ContentInfo>>, t: Throwable) {
@@ -47,10 +49,47 @@ class ContentsActivity : AppCompatActivity() {
             }
         })
 
+        //돌아가기 버튼
         btn_contents_back.setOnClickListener{
             finish()
         }
     }
+    inner class ContentsViewPagerAdapter(contentsList: ArrayList<ContentInfo>) : RecyclerView.Adapter<ContentsViewPagerAdapter.PagerViewHolder>() {
+        var item = contentsList
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = PagerViewHolder((parent))
 
 
+        override fun getItemCount(): Int = item.size
+
+        override fun onBindViewHolder(holder: PagerViewHolder, position: Int) {
+            //각 item에 저장된 리스트 값 지정
+            holder.id.text = item[position].c_id.toString()
+            holder.genre.text = item[position].c_genre.toString()
+            holder.name.text = item[position].c_name.toString()
+            holder.release.text = item[position].c_release_date.toString()
+            holder.cast.text = item[position].c_cast.toString()
+            holder.ageLimit.text = item[position].c_age_limit.toString()
+            holder.rating.text = item[position].c_rating.toString()
+            holder.platform.text = item[position].c_platform.toString()
+            holder.information.text = item[position].c_information.toString()
+            holder.previewURL.text = item[position].c_preview_url.toString()
+            holder.number.text = item[position].c_number.toString()
+        }
+
+        inner class PagerViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.content_item, parent, false)) {
+            //이너 클래스의 변수값 -> layout id 값과 연결
+            val id = itemView.content_id
+            val genre = itemView.content_genre
+            val name = itemView.content_name
+            val release = itemView.content_release
+            val cast = itemView.content_cast
+            val ageLimit = itemView.content_age_limit
+            val rating = itemView.content_rating
+            val platform = itemView.content_platform
+            val information = itemView.content_information
+            val previewURL = itemView.content_url
+            val number = itemView.content_number
+        }
+    }
 }
--- OOSOOSOO/app/src/main/java/kr/ac/kpu/oosoosoo/ReviewActivity.kt
+++ OOSOOSOO/app/src/main/java/kr/ac/kpu/oosoosoo/ReviewActivity.kt
@@ -7,5 +7,8 @@ class ReviewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_review)
+
+        //Contents 리뷰..
+        //22.01.28 Database 진행상황에 맞춰 화면 설정 (디자인 작업을 제외한 나머지 복붙 작업)
     }
 }
