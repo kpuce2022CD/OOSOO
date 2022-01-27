d495d9292d695c951124787bdb32798d21900d69
Jihn0118 <87293724+Jihn0118@users.noreply.github.com>
2022-01-28 04:52:45 +0900

    DB tmdb table 수정

--- Database/TMDB table 생성 쿼리.sql
+++ Database/TMDB table 생성 쿼리.sql
@@ -14,7 +14,7 @@ create table contents_movie (
 	homepage varchar(200), 
 	poster_path varchar(200), 
 	runtime int, 
-	overview varchar(1000), 
+	overview VARCHAR(2000), 
 	flatrate varchar(100), 
 	rent varchar(100), 
 	buy varchar(100)
@@ -27,7 +27,7 @@ CREATE TABLE contents_tv(
 	production_countries varchar(200) NOT NULL,
 	vote_average float(3,1) not null, 
 	vote_count int not null, 
-	overview VARCHAR(1000) NOT NULL,
+	overview VARCHAR(2000) NOT NULL,
 	homepage VARCHAR(200) NOT NULL,
 	number_of_seasons INT NOT NULL,
 	number_of_episodes INT NOT NULL,
@@ -39,4 +39,54 @@ CREATE TABLE contents_tv(
 	rent varchar(100), 
 	buy varchar(100)
 );
+
+
+CREATE TABLE users(
+id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+email VARCHAR(50) NOT NULL,
+passwd VARCHAR(20) NOT NULL,
+NAME VARCHAR(20) NOT NULL,
+phone_number VARCHAR(20) NOT NULL,
+nickname VARCHAR(20) NOT NULL,
+gender BOOLEAN NOT NULL,
+age INT NOT NULL,
+job VARCHAR(20),
+overview VARCHAR(200),
+coin INT
+);
+
+CREATE TABLE user_interworking(
+i_id VARCHAR(50) NOT NULL PRIMARY KEY,
+id VARCHAR(50) NOT NULL,
+passwd VARCHAR(20) NOT NULL,
+profile_name VARCHAR(20) NOT NULL,
+expiration_date DATE NOT NULL,
+simple_login VARCHAR(20)
+);
+
+CREATE TABLE movie_review(
+id INT NOT NULL PRIMARY KEY,
+m_id INT NOT NULL,
+u_id INT NOT NULL,
+_like BOOLEAN NOT NULL,
+rating FLOAT(3,1) NOT NULL,
+review VARCHAR(1000) NOT NULL,
+_datetime DATETIME NOT NULL,
+FOREIGN KEY(m_id) REFERENCES contents_movie(id),
+FOREIGN KEY(u_id) REFERENCES users(id)
+);
+
+CREATE TABLE tv_review(
+id INT NOT NULL PRIMARY KEY,
+t_id INT NOT NULL,
+u_id INT NOT NULL,
+_like BOOLEAN NOT NULL,
+rating FLOAT(3,1) NOT NULL,
+review VARCHAR(1000) NOT NULL,
+_datetime DATETIME NOT NULL,
+FOREIGN KEY(t_id) REFERENCES contents_tv(id),
+FOREIGN KEY(u_id) REFERENCES users(id)
+);
+
 	
+
