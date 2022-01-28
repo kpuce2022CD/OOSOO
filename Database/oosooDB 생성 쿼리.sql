b9134324870cbb5b6f07e38dfafcc84eb6349962
Jihn0118 <87293724+Jihn0118@users.noreply.github.com>
2022-01-29 01:52:18 +0900

    oosooDB 수정

---
+++ Database/oosooDB 생성 쿼리.sql
@@ -0,0 +1,64 @@
+CREATE DATABASE oosooDB DEFAULT CHARACTER SET UTF8 COLLATE UTF8_general_ci;
+
+USE oosooDB;
+
+CREATE TABLE contents(
+	id VARCHAR(100) NOT NULL PRIMARY KEY, 
+    _type VARCHAR(10) NOT NULL,
+	title VARCHAR(100) NOT NULL, 
+	genre VARCHAR(100) NOT NULL, 
+	production_countries VARCHAR(200) NOT NULL, 
+	vote_count INT NOT NULL, 
+	vote_average FLOAT(3,1) NOT NULL, 
+    number_of_seasons INT,
+    number_of_episodes INT,
+	release_date DATE, 
+	adult BOOLEAN, 
+	poster_path VARCHAR(200), 
+	runtime INT, 
+	overview VARCHAR(2000), 
+    now_status VARCHAR(100),
+	flatrate VARCHAR(100), 
+	rent VARCHAR(100), 
+	buy VARCHAR(100)
+);
+
+
+
+CREATE TABLE users(
+id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+email VARCHAR(50) NOT NULL,
+passwd VARCHAR(20) NOT NULL,
+name VARCHAR(20) NOT NULL,
+phone_number VARCHAR(20) NOT NULL,
+nickname VARCHAR(20) NOT NULL,
+gender BOOLEAN NOT NULL,
+age INT NOT NULL,
+job VARCHAR(20),
+overview VARCHAR(200),
+coin INT
+);
+
+CREATE TABLE user_interworking(
+i_id VARCHAR(50) NOT NULL PRIMARY KEY,
+u_id INT NOT NULL,
+platform VARCHAR(20) NOT NULL,
+id VARCHAR(50) NOT NULL,
+passwd VARCHAR(20) NOT NULL,
+profile_name VARCHAR(20) NOT NULL,
+expiration_date DATE NOT NULL,
+simple_login VARCHAR(20),
+FOREIGN KEY(u_id) REFERENCES users(id)
+);
+
+CREATE TABLE contents_review(
+id INT NOT NULL PRIMARY KEY,
+c_id varchar(100) NOT NULL,
+u_id INT NOT NULL,
+_like BOOLEAN NOT NULL,
+rating FLOAT(3,1) NOT NULL,
+review VARCHAR(1000) NOT NULL,
+_datetime DATETIME NOT NULL,
+FOREIGN KEY(c_id) REFERENCES contents(id),
+FOREIGN KEY(u_id) REFERENCES users(id)
+);
