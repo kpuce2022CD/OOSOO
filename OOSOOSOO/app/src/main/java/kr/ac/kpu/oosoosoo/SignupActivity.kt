aefe569760169b26e9208195e98ad5331a766f24
eoeo0326 <eoeo0326@gmail.com>
2022-02-07 19:11:11 +0900

    Signup Activity 생성

--- OOSOOSOO/app/src/main/java/kr/ac/kpu/oosoosoo/SignupActivity.kt
+++ OOSOOSOO/app/src/main/java/kr/ac/kpu/oosoosoo/SignupActivity.kt
@@ -3,13 +3,9 @@ package kr.ac.kpu.oosoosoo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class LoginActivity : AppCompatActivity() {
+class SignupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
-
-
-        
-
+        setContentView(R.layout.activity_signup)
     }
 }
