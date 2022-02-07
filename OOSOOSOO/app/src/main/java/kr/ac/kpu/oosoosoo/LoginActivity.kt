7b55dab00fb22527b8168d34ee9977e63cc7e658
eoeo0326 <eoeo0326@gmail.com>
2022-02-07 19:17:48 +0900

    Signup Activity 연결

--- OOSOOSOO/app/src/main/java/kr/ac/kpu/oosoosoo/LoginActivity.kt
+++ OOSOOSOO/app/src/main/java/kr/ac/kpu/oosoosoo/LoginActivity.kt
@@ -2,14 +2,18 @@ package kr.ac.kpu.oosoosoo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
+import org.jetbrains.anko.startActivity
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+    signup_btn.setOnClickListener {
+        startActivity<SignupActivity>()
+    }
 
-        
 
     }
 }
