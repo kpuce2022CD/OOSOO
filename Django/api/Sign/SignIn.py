93990df22075607d1b676906b12eced0c00edc08
Jihn0118 <87293724+Jihn0118@users.noreply.github.com>
2022-02-08 19:46:36 +0900

    Sign 폴더 생성

---
+++ Django/api/Sign/SignIn.py
@@ -0,0 +1,18 @@
+import mariadb
+import sys
+
+# Connect to MariaDB Platform
+try:
+    conn = mariadb.connect(
+        user="jinho",
+        password="2017150011",
+        host="oosoords.cqo7bp08tx6h.ap-northeast-2.rds.amazonaws.com",
+        port=3306
+    )
+
+except mariadb.Error as e:
+    print(f"failed: Error connecting to Mariadb: {e}")
+    sys.exit(1)
+
+
+conn.close()
