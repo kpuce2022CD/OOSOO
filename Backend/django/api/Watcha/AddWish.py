fbff68b95a5746f2a6b5bb4c7faaaee01a787865
eoeo0326 <eoeo0326@gmail.com>
2022-02-21 00:41:21 +0900

    Watcha Addwish 오류 해결

--- Backend/django/api/Watcha/AddWish.py
+++ Backend/django/api/Watcha/AddWish.py
@@ -10,13 +10,13 @@ import time
 
 
 def w_addwish(email, pwd, name, title):
-    #display = Display(visible=0, size=(1920, 1080))  # PyCharm 테스트시 주석처리
-    #display.start()  # PyCharm 테스트시 주석처리
+    display = Display(visible=0, size=(1920, 1080))  # PyCharm 테스트시 주석처리
+    display.start()  # PyCharm 테스트시 주석처리
 
     # chrome창(웹드라이버) 열기  (Docker 경로 : "/webserver/chromedriver")
     path = "/webserver/chromedriver"    # PyCharm 테스트시  r"D:\2022 Capston\OOSOO\Python\Watcha\chromedriver.exe"
     options = webdriver.ChromeOptions()
-    options.add_argument('--headless')
+    #options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('disable-gpu')
     options.add_argument('--disable-dev-shm-usage')
@@ -46,19 +46,22 @@ def w_addwish(email, pwd, name, title):
             result.click()
             time.sleep(2)
             try:
-                driver.find_element_by_class_name('css-zuimwg').click()
+                btn_addwish = driver.find_element_by_class_name('css-zuimwg')
+                print(btn_addwish.text)
+                btn_addwish.click()
                 time.sleep(2)
                 driver.quit()
-                #display.stop()
+                display.stop()
+                time.sleep(2)
                 return "success"
             except:
                 driver.quit()
-                #display.stop()
+                display.stop()
                 return "fail"
         else:
             continue
 
     driver.quit()
-    #display.stop()
+    display.stop()
 
     return "fail"
