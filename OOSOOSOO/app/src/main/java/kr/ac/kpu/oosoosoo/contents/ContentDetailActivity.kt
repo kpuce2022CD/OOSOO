87ccb0593f0daa235d88fc71c76b32b52ea81255
eoeo0326 <eoeo0326@gmail.com>
2022-02-25 23:57:00 +0900

    Content Detail : Genre, Overview 출력 디자인 개선

--- OOSOOSOO/app/src/main/java/kr/ac/kpu/oosoosoo/contents/ContentDetailActivity.kt
+++ OOSOOSOO/app/src/main/java/kr/ac/kpu/oosoosoo/contents/ContentDetailActivity.kt
@@ -24,12 +24,29 @@ class ContentDetailActivity : AppCompatActivity() {
                 .placeholder(android.R.drawable.ic_popup_sync)
                 .into(img_poster_detail)
 
-            // 제목, 연도, 장르
+            // 제목
             tv_title_detail.text = contentInfo.title
-            tv_date_genre.text = contentInfo.release_date + " · " + contentInfo.genre
+
+            // 날짜
+            tv_date_genre.text = contentInfo.release_date + " · "
+
+            // 장르
+            val genre = contentInfo.genre?.split(",")
+            if (genre != null) {
+                for (i in 0..(genre.size - 3)) {
+                    tv_date_genre.text = tv_date_genre.text.toString() + genre[i] + " /"
+                }
+                tv_date_genre.text = tv_date_genre.text.toString() + genre[genre.size - 2]
+            }
 
             // Overview
-            tv_overview_detail.text = contentInfo.overview
+            val overview = contentInfo.overview?.split(". ")
+            if (overview != null) {
+                for (i in 0..(overview.size - 2)) {
+                    tv_overview_detail.text = tv_overview_detail.text.toString() + overview[i] + ".\n\n"
+                }
+                tv_overview_detail.text = tv_overview_detail.text.toString() + overview[overview.size - 1]
+            }
 
             // 더보기 기능
             tv_overview_detail.post {
