79b283c751f834d36e591dd84dc352527e3676be
hoho-97 <cksgh5652@gmail.com>
2022-02-28 17:43:26 +0900

    Android : 홈화면 로그인, 검색 버튼 추가

--- OOSOOSOO/app/src/main/java/kr/ac/kpu/oosoosoo/home/HomeActivity.kt
+++ OOSOOSOO/app/src/main/java/kr/ac/kpu/oosoosoo/home/HomeActivity.kt
@@ -2,21 +2,13 @@ package kr.ac.kpu.oosoosoo.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import androidx.recyclerview.widget.LinearLayoutManager
-import kotlinx.android.synthetic.main.activity_home.*
-import kotlinx.android.synthetic.main.fragment_home_platform.*
 import kr.ac.kpu.oosoosoo.R
-import kr.ac.kpu.oosoosoo.adapters.ContentCardListAdapter
-import kr.ac.kpu.oosoosoo.connection.RetrofitBuilder
-import kr.ac.kpu.oosoosoo.contents.ContentInfo
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
     }
+
+
 }
--- OOSOOSOO/app/src/main/java/kr/ac/kpu/oosoosoo/home/HomeFragment.kt
+++ OOSOOSOO/app/src/main/java/kr/ac/kpu/oosoosoo/home/HomeFragment.kt
@@ -6,11 +6,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.android.synthetic.main.fragment_home.*
+import kotlinx.android.synthetic.main.fragment_home.view.*
 import kr.ac.kpu.oosoosoo.R
 import kr.ac.kpu.oosoosoo.adapters.PagerFragmentStateAdapter
+import org.jetbrains.anko.toast
 
 class HomeFragment : Fragment() {
 
@@ -21,13 +24,27 @@ class HomeFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view: View = inflater!!.inflate(R.layout.fragment_home, container, false)
+
+        //홈화면 검색 돋보기 버튼
+        view.btn_home_search.setOnClickListener {
+            requireContext().toast("Search Click")
+        }
+
+        //홈화면 로그인 버튼
+        view.btn_home_login.setOnClickListener{
+            //로그인 시 이미지 변환 필요
+            requireContext().toast("Login Click")
+        }
+
+        return view
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
