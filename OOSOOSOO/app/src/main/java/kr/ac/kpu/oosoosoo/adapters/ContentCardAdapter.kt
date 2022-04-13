bbc7e6850941e9d6b0b9a0bb0123d68329cebd0d
박찬호 <cksgh5652@gmail.com>
2022-04-14 05:19:56 +0900

    Android : 슬라이드 loadMore 코드 변경

--- OOSOOSOO/app/src/main/java/kr/ac/kpu/oosoosoo/adapters/ContentCardAdapter.kt
+++ OOSOOSOO/app/src/main/java/kr/ac/kpu/oosoosoo/adapters/ContentCardAdapter.kt
@@ -1,7 +1,7 @@
 package kr.ac.kpu.oosoosoo.adapters
 
 import android.content.Context
-import android.os.Handler
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,65 +9,93 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.recy_item_content_card.view.*
-import kotlinx.android.synthetic.main.recy_item_content_row.view.*
 import kr.ac.kpu.oosoosoo.R
+import kr.ac.kpu.oosoosoo.adapters.Constant.VIEW_TYPE_ITEM
+import kr.ac.kpu.oosoosoo.adapters.Constant.VIEW_TYPE_LOADING
 import kr.ac.kpu.oosoosoo.contents.ContentDetailActivity
 import kr.ac.kpu.oosoosoo.contents.ContentInfo
 import org.jetbrains.anko.startActivity
 
+
+
+
 //자식 컨테이너 어댑터
-class ContentCardAdapter(context: Context, cardListData: ArrayList<ContentInfo>?): RecyclerView.Adapter<ContentCardAdapter.ViewHolder>() {
+class ContentCardAdapter(context: Context, cardListData: ArrayList<ContentInfo>?): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     //출력할 하나의 item List
-    private var contentList : ArrayList<ContentInfo>? = cardListData
+    var contentList : ArrayList<ContentInfo>? = cardListData
 
     val context : Context = context
 
     //해당 컨텐츠 플랫폼 리스트
     var platformList : ArrayList<String>? = null
 
-    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    /*inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
         init {
 
         }
         //layout 파일에 값 출력
-        fun bind(result: ContentInfo, context: Context, adapter: ContentCardPlatformAdapter) {
-            itemView.card_item_title.text = result.title
+        fun bind(result: ContentInfo, adapter: ContentCardPlatformAdapter) {
 
-            val url = "https://image.tmdb.org/t/p/original" + contentList!![position].poster_path
+        }
+    }*/
+
+    //출력할 xml파일 지정
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return if (viewType == VIEW_TYPE_ITEM) {
+            val view: View = LayoutInflater.from(parent.context).inflate(R.layout.recy_item_content_card, parent, false)
+            ItemViewHolder(view)
+        } else {
+            val view: View =
+                LayoutInflater.from(parent.context).inflate(R.layout.recy_item_loading, parent, false)
+            LoadingViewHolder(view)
+        }
+    }
+
+    //bind 과정
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if(holder is ItemViewHolder) {
+            platformList = ArrayList(contentList!![position]!!.flatrate?.split(','))
+            val contentCardPlatformAdapter = ContentCardPlatformAdapter(context, platformList!!)
+            //holder.bind(contentList!![position]!!, contentCardPlatformAdapter)
+            holder.itemView.card_item_title.text = contentList!![position]!!.title
+
+            val url = "https://image.tmdb.org/t/p/original" + contentList!![position]!!.poster_path
 
             Glide.with(context)
                 .load(url)
-                .into(itemView.img_card_item)
+                .into(holder.itemView.img_card_item)
 
             //플랫폼 어댑터 지정(수평방향)
-            itemView.platform_recyclerview.layoutManager = LinearLayoutManager(itemView.context, LinearLayoutManager.HORIZONTAL,false)
-            itemView.platform_recyclerview.adapter = adapter
-        }
-    }
+            holder.itemView.platform_recyclerview.layoutManager =
+                LinearLayoutManager(holder.itemView.context, LinearLayoutManager.HORIZONTAL, false)
+            holder.itemView.platform_recyclerview.adapter = contentCardPlatformAdapter
 
-    //출력할 xml파일 지정
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder(
-        LayoutInflater.from(parent.context).inflate(
-            R.layout.recy_item_content_card, parent,
-            false
-        )
-    )
+            holder.itemView.setOnClickListener {
+                context.startActivity<ContentDetailActivity>(
+                    "content" to contentList!![position]
+                )
+            }
 
-    //bind 과정
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val platformList = ArrayList(contentList!![position].flatrate?.split(','))
-        val contentCardPlatformAdapter = ContentCardPlatformAdapter(context, platformList!!)
-        holder.bind(contentList!![position], context, contentCardPlatformAdapter)
-        holder.itemView.setOnClickListener {
-            context.startActivity<ContentDetailActivity>(
-                "content" to contentList!![position]
-            )
+        } else {
+            Log.d("testest", contentList.toString())
         }
 
     }
 
-    override fun getItemCount(): Int = contentList!!.size
+    override fun getItemCount(): Int {
+        return if (contentList == null) 0 else contentList!!.size
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if (contentList!![position] == null) VIEW_TYPE_LOADING else VIEW_TYPE_ITEM
+    }
+
+    private class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    }
+
+    private class LoadingViewHolder(view: View) : RecyclerView.ViewHolder(view) { }
 
 }
--- OOSOOSOO/app/src/main/java/kr/ac/kpu/oosoosoo/adapters/ContentCardListAdapter.kt
+++ OOSOOSOO/app/src/main/java/kr/ac/kpu/oosoosoo/adapters/ContentCardListAdapter.kt
@@ -5,9 +5,7 @@ import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.size
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.recy_item_content_row.view.*
 import kr.ac.kpu.oosoosoo.R
@@ -21,8 +19,11 @@ class ContentCardListAdapter(context: Context, cardRowData: ArrayList<CardListDa
 
     //출력할 하나의 item List
     private var contentRowList : ArrayList<CardListData> = cardRowData!!
-
     val context : Context = context
+
+    //로딩시 버퍼 item List
+    private var contentBufferList: ArrayList<ContentInfo?> = ArrayList()
+    var isLoading = false
 
     var onItemClick: ((CardListData) -> Unit)? = null
 
@@ -38,10 +39,37 @@ class ContentCardListAdapter(context: Context, cardRowData: ArrayList<CardListDa
         fun bind(result: CardListData, adapter: ContentCardAdapter) {
             itemView.item_row_title.text = result.cardListTitle
             //자식 어댑터 지정(수평방향)
-            itemView.card_recyclerview.layoutManager = GridLayoutManager(itemView.context, 2,GridLayoutManager.HORIZONTAL,false)
-            //itemView.card_recyclerview.setHasFixedSize(true)
+            val gridLayoutManager = GridLayoutManager(itemView.context, 2,GridLayoutManager.HORIZONTAL,false)
+            itemView.card_recyclerview.layoutManager = gridLayoutManager
+            itemView.card_recyclerview.setHasFixedSize(true)
             itemView.card_recyclerview.adapter = adapter
+            gridLayoutManager.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
+                override fun getSpanSize(position: Int): Int {
+                    return when (adapter.getItemViewType(position)) {
+                        VIEW_TYPE_ITEM -> 1
+                        VIEW_TYPE_LOADING -> 2 //number of columns of the grid
+                        else -> -1
+                    }
+                }
+            }
 
+
+            /*itemView.card_recyclerview.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+                override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                    super.onScrolled(recyclerView, dx, dy)
+                    val gridLayoutManager = recyclerView.layoutManager as GridLayoutManager?
+
+                    if (!isLoading) {
+                        Toast.makeText(context, "hello", Toast.LENGTH_SHORT).show()
+                        if (gridLayoutManager != null && gridLayoutManager.findLastCompletelyVisibleItemPosition() == contentBufferList.size - 1) {
+                            //추가 후 로딩
+                            loadMore(adapter.contentList!!, adapter)
+                            Log.d("testest", "test")
+                            isLoading = true
+                        }
+                    }
+                }
+            })*/
         }
 
 
@@ -58,9 +86,28 @@ class ContentCardListAdapter(context: Context, cardRowData: ArrayList<CardListDa
 
     //bind 과정
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val contentCardAdapter = ContentCardAdapter(context, contentRowList[position].cardItemList)
+        val contentCardAdapter = ContentCardAdapter(context, contentRowList!![position].cardItemList)
         holder.bind(contentRowList[position], contentCardAdapter)
     }
 
     override fun getItemCount(): Int = contentRowList.size
+
+    private fun loadMore(contentItemList: ArrayList<ContentInfo?>, adapter: ContentCardAdapter) {
+        contentBufferList.add(contentItemList[0])
+        adapter.notifyItemInserted(contentBufferList.size - 1)
+        val handler = Handler()
+        handler.postDelayed({
+            contentBufferList.removeAt(contentBufferList.size - 1)
+            val scrollPosition: Int = contentBufferList.size
+            adapter.notifyItemRemoved(scrollPosition)
+            var currentSize = scrollPosition
+            val nextLimit = currentSize + 10
+            while (currentSize < nextLimit + 1) {
+                contentBufferList.add(contentItemList[currentSize])
+                currentSize++
+            }
+            adapter.notifyDataSetChanged()
+            isLoading = false
+        }, 20)
+    }
 }
