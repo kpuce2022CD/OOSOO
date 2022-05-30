a4e437716bb601fdc08b5726f636b0b2cc4e5685
eoeo0326 <eoeo0326@gmail.com>
2022-05-30 19:02:36 +0900

    Database : People 테이블 데이터 추가

--- Python/DB/Input_People_DB.py
+++ Python/DB/Input_People_DB.py
@@ -1,13 +1,17 @@
 import requests     # aiohttp로 대체
 import aiohttp
+import mariadb
 import json
-import sys
 import time
 import asyncio
-from threading import Thread
 
-data = []
-ids = []
+
+conn = mariadb.connect(
+    user="jinho",
+    password="2017150011",
+    host="oosoords.cqo7bp08tx6h.ap-northeast-2.rds.amazonaws.com",
+    port=3306
+)
 
 
 async def get_people(num):
@@ -17,37 +21,44 @@ async def get_people(num):
         async with aiohttp.ClientSession() as session:
             async with session.get(url) as response:
                 people_json = json.loads(await response.text())
-                if people_json['popularity'] > 2.0:
-                    data.append(people_json)
-                    ids.append(num)
-                    print('id : ' + str(num))
-                    print(people_json['name'])
-                    print(people_json['birthday'])
-                    print(people_json['gender'])
-                    print(people_json['known_for_department'])
-                    print(people_json['popularity'])
-                    print(people_json['profile_path'])
-                    print('\n')
-    except:
-        print('except \n')
-        pass
+                if people_json['popularity'] > 2.0 \
+                        and (people_json['known_for_department'] == 'Acting' or people_json['known_for_department'] == 'Directing'):
+                    print('id : ' + str(people_json['id']))
+                    id = people_json['id']
+                    name = people_json['name']
+                    birthday = people_json['birthday']
+                    department = people_json['known_for_department']
+                    popularity = people_json['popularity']
+                    profile_path = people_json['profile_path']
+                    try:
+                        cur = conn.cursor()
+                        use_db_query = "use oosooDB"
+                        insert_query = "INSERT INTO people(id, name, birthday, department, popularity, profile_path) VALUES(?, ?, ?, ?, ?, ?) "
+
+                        cur.execute(use_db_query)
+                        cur.execute(insert_query, (id, name, birthday, department, popularity, profile_path))
+                        conn.commit()
+                        print('id=' + str(id) + ' add')
+
+                    except mariadb.Error as e:
+                        print(f"Error: {e}")
+                        print(id)
+    except Exception as e:
+        print(str(num) + ' except ', e)
 
 
 asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 start = time.time()
 
-tasks = [get_people(i) for i in range(0, 100)]
-asyncio.run(asyncio.wait(tasks))
+
+# latest = 3569716
+# 0 ~ 999
+for i in range(0, 10):
+    tasks = [get_people(k) for k in range(i*100, i*100+100)]
+    asyncio.run(asyncio.wait(tasks))
+    #print('\nsleep 20sec\n')
+    #time.sleep(5)
 
 end = time.time()
 print(f'async : {end - start}')
-
-print('data length : ' + str(len(data)))
-print('ids length : ' + str(len(ids)))
-
-for people in data:
-    print('data[\'id\'] : ' + str(people['id']))
-
-for num in ids:
-    print('id : ' + str(num))
