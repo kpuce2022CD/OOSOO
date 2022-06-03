672b5ab63dae75aaf558e64e2dd20880c7a4e0eb
Jihn0118 <lantern50@tukorea.ac.kr>
2022-06-03 16:43:14 +0900

    Backend: wavve 연동 오류 수정

--- Backend/django/api/Contents/ott_link.py
+++ Backend/django/api/Contents/ott_link.py
@@ -1,4 +1,5 @@
 # selenium 불러오기
+import selenium.webdriver.remote.errorhandler
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -16,17 +17,22 @@ def netflix_url(title, driver): #제목으로 netflix에 있는 해당 컨텐츠
     url_home = "https://www.netflix.com/kr/"
     driver.get(url_home)
 
-    URL = "https://www.netflix.com/search?q=" + title
-    driver.get(URL)
+    try:
+        URL = "https://www.netflix.com/search?q=" + title
+        driver.get(URL)
 
-    first_content = driver.find_element(By.XPATH, '//*[@id="row-0"]/div/div/div/div/div/div[1]')
-    first_content.click()
+        first_content = driver.find_element(By.XPATH, '//*[@id="row-0"]/div/div/div/div/div/div[1]')
+        first_content.click()
 
-    btn_watch = driver.find_element(By.XPATH, '//*[@id="appMountPoint"]/div/div/div[1]/div[2]/div/div[1]/div[4]/div/div[1]/div[1]/a/button')
-    btn_watch.click()
+        btn_watch = driver.find_element(By.XPATH,
+                                        '//*[@id="appMountPoint"]/div/div/div[1]/div[2]/div/div[1]/div[4]/div/div[1]/div[1]/a/button')
+        btn_watch.click()
 
-    time.sleep(2)
-    return driver.current_url
+        time.sleep(2)
+        return driver.current_url
+    except:
+        return "x"
+
 
 
 def disney_url(title, driver): #제목으로 disney plus에 있는 해당 컨텐츠 시청 url 리턴
@@ -34,128 +40,146 @@ def disney_url(title, driver): #제목으로 disney plus에 있는 해당 컨텐
     time.sleep(2)
     driver.implicitly_wait(5)
 
-    btn_search = driver.find_element(By.XPATH, '//*[@id="nav-list"]/span[2]')
-    btn_search.click()
-
-    input_bar = driver.find_element(By.XPATH, '//*[@id="search-input"]')
-    input_bar.send_keys(title)
-
-    content = driver.find_element(By.XPATH, '//*[@id="section_index"]/div/div/div[2]/section/div/div/div[1]/a')
-    content.click()
-    time.sleep(2)
-    driver.implicitly_wait(5)
-
-    #시청 버튼 예외 처리 - 해당 콘텐츠 디테일의 줄 수에 따라 버튼의 XPATH가 바뀜
     try:
-        btn_watch = driver.find_element(By.XPATH, '//*[@id="details_index"]/div/article/div[3]/div/div[2]/button[1]')
+        btn_search = driver.find_element(By.XPATH, '//*[@id="nav-list"]/span[2]')
+        btn_search.click()
+
+        input_bar = driver.find_element(By.XPATH, '//*[@id="search-input"]')
+        input_bar.send_keys(title)
+
+        content = driver.find_element(By.XPATH, '//*[@id="section_index"]/div/div/div[2]/section/div/div/div[1]/a')
+        content.click()
+        time.sleep(2)
+        driver.implicitly_wait(5)
+
+        # 시청 버튼 예외 처리 - 해당 콘텐츠 디테일의 줄 수에 따라 버튼의 XPATH가 바뀜
+        try:
+            btn_watch = driver.find_element(By.XPATH,
+                                            '//*[@id="details_index"]/div/article/div[3]/div/div[2]/button[1]')
+        except:
+            btn_watch = driver.find_element(By.XPATH, '//*[@id="details_index"]/div/article/div[3]/div/div[3]/button')
+
+        btn_watch.click()
+        time.sleep(2)
+        driver.implicitly_wait(5)
+
+        return driver.current_url
+
     except:
-        btn_watch = driver.find_element(By.XPATH, '//*[@id="details_index"]/div/article/div[3]/div/div[3]/button')
-
-    btn_watch.click()
-    time.sleep(2)
-    driver.implicitly_wait(5)
-
-    return driver.current_url
+        return "x"
 
 
-def wavve_url(title, driver): #제목으로 wavve에 있는 해당 컨텐츠 시청 url 리턴
+def wavve_url(title, driver, c_type): #제목으로 wavve에 있는 해당 컨텐츠 시청 url 리턴
 
     xbox = driver.find_element(By.XPATH, '//*[@id="contents"]/div[1]/section/div[1]')
     xbox.click()
 
-    btn_search = driver.find_element(By.XPATH, '//*[@id="app"]/div[1]/div[2]/header/div[2]/div/button')
-    btn_search.click()
+    try:
+        ##Content = TV일 때
+        if c_type == "tv":
+            tv_search_url = "https://www.wavve.com/search/search?category=program&searchWord=" + title
+            driver.get(tv_search_url)
+            time.sleep(2)
+            driver.implicitly_wait(5)
 
-    input_title = driver.find_element(By.XPATH, '//*[@id="search-ip"]')
-    input_title.send_keys(title)
-    input_title.send_keys(Keys.RETURN)
-    time.sleep(2)
-    driver.implicitly_wait(5)
+        ##Content = Movie일 때
+        elif c_type == "movie":
+            movie_search_url = "https://www.wavve.com/search/search?category=movie&searchWord=" + title
+            driver.get(movie_search_url)
+            time.sleep(2)
+            driver.implicitly_wait(5)
 
-    xbox2 = driver.find_element(By.XPATH, '//*[@id="contents"]/div[1]/div/a')
-    xbox2.click()
+        xbox2 = driver.find_element(By.XPATH, '//*[@id="contents"]/div[1]/div/a')
+        xbox2.click()
+        time.sleep(2)
+        driver.implicitly_wait(5)
 
-    first_content = driver.find_element(By.XPATH, '//*[@id="multisection_index_undefined"]/div[2]/div[1]/div[1]/div')
-    first_content.click()
-    time.sleep(2)
-    driver.implicitly_wait(5)
+        first_content = driver.find_element(By.XPATH, '//*[@id="contents"]/div[3]/div/div/div/div[2]/div[1]/a/div[1]')
+        first_content.click()
+        time.sleep(2)
+        driver.implicitly_wait(5)
 
-    return driver.current_url
+        return driver.current_url
+
+    except:
+        return "x"
 
 
 def watcha_url(title, driver, c_type): #제목으로 watcha에 있는 해당 컨텐츠 시청 url 리턴
-    ##Content = TV일 때
-    if c_type == "tv":
-        tv_search_url = "https://watcha.com/browse/search?query=" + title + "&filter=tv_search"
-        driver.get(tv_search_url)
-        time.sleep(5)
+    try:
+        ##Content = TV일 때
+        if c_type == "tv":
+            tv_search_url = "https://watcha.com/browse/search?query=" + title + "&filter=tv_search"
+            driver.get(tv_search_url)
+            time.sleep(5)
+            driver.implicitly_wait(5)
+
+            tv_content = driver.find_element(By.XPATH,
+                                             '//*[@id="root"]/div[1]/main/div[1]/section/div[1]/ul/li[1]/article[1]/a')
+            tv_content.click()
+            time.sleep(5)
+            driver.implicitly_wait(5)
+
+        ##Content = Movie일 때
+        elif c_type == "movie":
+            movie_search_url = "https://watcha.com/browse/search?query=" + title + "&filter=movie_search"
+            driver.get(movie_search_url)
+            time.sleep(5)
+            driver.implicitly_wait(5)
+
+            movie_content = driver.find_element(By.XPATH,
+                                                '//*[@id="root"]/div[1]/main/div[1]/section/div/ul/li[1]/article[1]/a')
+            movie_content.click()
+            time.sleep(5)
+            driver.implicitly_wait(5)
+
+        time.sleep(2)
         driver.implicitly_wait(5)
 
-        tv_content = driver.find_element(By.XPATH, '//*[@id="root"]/div[1]/main/div[1]/section/div[1]/ul/li[1]/article[1]/a')
-        tv_content.click()
-        time.sleep(5)
+        btn_watch = driver.find_element(By.XPATH,
+                                        '//*[@id="root"]/div[1]/main/div[1]/header[1]/div/div/section/div[1]/a')
+        btn_watch.click()
+
+        time.sleep(2)
         driver.implicitly_wait(5)
 
-    ##Content = Movie일 때
-    elif c_type == "movie":
-        movie_search_url = "https://watcha.com/browse/search?query=" + title + "&filter=movie_search"
-        driver.get(movie_search_url)
-        time.sleep(5)
-        driver.implicitly_wait(5)
+        return driver.current_url
 
-        movie_content = driver.find_element(By.XPATH, '//*[@id="root"]/div[1]/main/div[1]/section/div/ul/li[1]/article[1]/a')
-        movie_content.click()
-        time.sleep(5)
-        driver.implicitly_wait(5)
+    except:
+        return "x"
 
-    else:
-        return "error"
-
-    time.sleep(2)
-    driver.implicitly_wait(5)
-
-    btn_watch = driver.find_element(By.XPATH, '//*[@id="root"]/div[1]/main/div[1]/header[1]/div/div/section/div[1]/a')
-    btn_watch.click()
-
-    time.sleep(2)
-    driver.implicitly_wait(5)
-
-    driver.save_screenshot("screenshot_ott_link_watcha.png")
-    return driver.current_url
 
 def tving_url(title, driver, c_type): #제목으로 tving에 있는 해당 컨텐츠 시청
                                       # url 리턴 제목서칭이 이상함!!!!!!!!
-    if c_type == "tv":
-        search_url = "https://www.tving.com/search/tv?keyword=" + title
-        driver.get(search_url)
+    try:
+        if c_type == "tv":
+            search_url = "https://www.tving.com/search/tv?keyword=" + title
+            driver.get(search_url)
+            time.sleep(2)
+            driver.implicitly_wait(5)
+
+        elif c_type == "movie":
+            search_url = "https://www.tving.com/search/movie?keyword=" + title
+            driver.get(search_url)
+            time.sleep(2)
+            driver.implicitly_wait(5)
+
+        first_content = driver.find_element(By.XPATH,
+                                            '//*[@id="__next"]/main/section/div/div/section/section/div[2]/div[1]')
+        first_content.click()
         time.sleep(2)
         driver.implicitly_wait(5)
-        driver.save_screenshot("test_tv1.png")
 
-    elif c_type == "movie":
-        search_url = "https://www.tving.com/search/movie?keyword=" + title
-        driver.get(search_url)
+        btn_watch = driver.find_element(By.XPATH,
+                                        '//*[@id="__next"]/main/section/article/article/div[2]/div[2]/div[2]/button')
+        btn_watch.click()
         time.sleep(2)
         driver.implicitly_wait(5)
-        driver.save_screenshot("test_movie1.png")
 
-    else:
-        return "error"
+        return driver.current_url
 
-    first_content = driver.find_element(By.XPATH, '//*[@id="__next"]/main/section/div/div/section/section/div[2]/div[1]')
-    first_content.click()
-    time.sleep(2)
-    driver.implicitly_wait(5)
-
-    driver.save_screenshot("test5.png")
-
-    btn_watch = driver.find_element(By.XPATH, '//*[@id="__next"]/main/section/article/article/div[2]/div[2]/div[2]/button')
-    btn_watch.click()
-    time.sleep(2)
-    driver.implicitly_wait(5)
-    driver.save_screenshot("test6.png")
-
-    return driver.current_url
+    except:
+        return "x"
 
 
 def ott_link(email, title, c_type, platform):
@@ -192,12 +216,11 @@ def ott_link(email, title, c_type, platform):
 
     elif platform == "wavve":
         wav_login(i['id'], i['passwd'], i['profile_name'], driver)
-        link = wavve_url(title, driver)
+        link = wavve_url(title, driver, c_type)
         return link
 
     elif platform == "watcha":
         wat_login(i['id'], i['passwd'], i['profile_name'], driver)
-        #await asyncio.wait([w_login(i['id'], i['passwd'], i['profile_name'], driver)])
         link = watcha_url(title, driver, c_type)
         return link
 
