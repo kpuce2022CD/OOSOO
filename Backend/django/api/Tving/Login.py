924f201b29506b0e86eec214c9c7a2551c7bcf8c
Jihn0118 <lantern50@tukorea.ac.kr>
2022-06-22 00:42:19 +0900

    Backend: Tving Login Cookie 추가

--- Backend/django/api/Tving/Login.py
+++ Backend/django/api/Tving/Login.py
@@ -1,38 +1,73 @@
 # selenium 불러오기
 from selenium.webdriver import ActionChains
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
+import pickle
+import glob
 import time
 
 def t_login(email, pwd, name, driver):
-    driver.get('https://user.tving.com/pc/user/otherLogin.tving?loginType=20&from=pc&rtUrl=https%3A%2F%2Fwww.tving.com&csite=&isAuto=false')
+    # 쿠키가 있는지 체크
+    find_cookie = False
+    cookie_files = glob.glob('./api/Cookies/' + email + '_t.pkl')
+    if len(cookie_files) > 0:
+        find_cookie = True
+    login_with_cookie = False
+    url = 'https://www.tving.com/'
 
-    # 로그인 액션체인
-    id_box = driver.find_element_by_css_selector('#a')
-    passwd_box = driver.find_element_by_css_selector('#b')
-    login_button = driver.find_element_by_css_selector('#doLoginBtn')
+    while (not login_with_cookie):
+        if find_cookie:  # 쿠키 정보를 이용해 로그인
+            cookies = pickle.load(
+                open('./api/Cookies/' + email + '_t.pkl', "rb"))
+            driver.get(url)
+            for c in cookies:
+                driver.add_cookie(c)
+            driver.get(url)
+            login_with_cookie = True
 
-    login_act = ActionChains(driver)
-    login_act.send_keys_to_element(id_box, email).send_keys_to_element(passwd_box, pwd).click(
-        login_button).perform()
+        else:  # 쿠키가 없을 경우, 수동으로 로그인하여 쿠키 정보 보존
+            try:
+                driver.get(
+                    'https://user.tving.com/pc/user/otherLogin.tving?loginType=20&from=pc&rtUrl=https%3A%2F%2Fwww.tving.com&csite=&isAuto=false')
 
-    time.sleep(3)
-    driver.implicitly_wait(5)
+                # 로그인 액션체인
+                id_box = driver.find_element_by_xpath('//*[@id="a"]')
+                passwd_box = driver.find_element_by_xpath('//*[@id="b"]')
+                login_button = driver.find_element_by_xpath('//*[@id="doLoginBtn"]')
 
-    #######################프로필 선택이 필요 없을 경우도 있음. 예외 처리 필요###########################
-    # 프로필 선택 화면 로딩이 완료될때까지 대기
-    driver.get("https://user.tving.com/pc/user/profiles.tving")
-    time.sleep(3)
-    driver.implicitly_wait(5)
+                login_act = ActionChains(driver)
+                login_act.send_keys_to_element(id_box, email).send_keys_to_element(passwd_box, pwd).click(
+                    login_button).perform()
 
-    # 프로필 선택
-    profiles = driver.find_elements_by_class_name('item')
-    for profile in profiles:
-        if profile.text == name:
-            profile.click()
-            break
+                time.sleep(2)
+                driver.implicitly_wait(5)
 
-    time.sleep(3)
-    driver.implicitly_wait(5)
+                #######################프로필 선택이 필요 없을 경우도 있음. 예외 처리 필요###########################
+                # 프로필 선택 화면 로딩이 완료될때까지 대기
+                driver.get("https://user.tving.com/pc/user/profiles.tving")
+                time.sleep(2)
+                driver.implicitly_wait(5)
+
+                # 프로필 선택
+                profiles = driver.find_elements_by_class_name('item')
+                for profile in profiles:
+                    if profile.text == name:
+                        profile.click()
+                        break
+
+                time.sleep(2)
+                driver.implicitly_wait(5)
+
+                driver.add_cookie({"name": email, "value": email})
+
+            except:
+                return "login error"
+
+            while (not find_cookie):
+                cookies = driver.get_cookies()
+                if len(cookies) > 0:
+                    find_cookie = True
+            pickle.dump(cookies, open('./api/Cookies/' + email + '_t.pkl', "wb"))
+            driver.close()
+            driver.quit()
+            return "normal login"
+
+    return "cookie login"
